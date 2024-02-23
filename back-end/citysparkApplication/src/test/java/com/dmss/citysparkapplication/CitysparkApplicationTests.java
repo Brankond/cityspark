f9e81691aa5fd8e5c7da6e5a0d91e9288166f2e2
LeoChenJR <leochenjinran@gmail.com>
2024-02-23 21:11:24 +0800

    create springboot application

---
+++ back-end/citysparkApplication/src/test/java/com/dmss/citysparkapplication/CitysparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmss.citysparkapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitysparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
