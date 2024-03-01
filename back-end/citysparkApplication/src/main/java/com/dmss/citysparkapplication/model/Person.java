067a6452dd8e6f5c12a9982514796f87bee41667
LeoChenJR <leochenjinran@gmail.com>
2024-03-02 01:29:09 +0800

    Add person components

---
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/model/Person.java
@@ -0,0 +1,29 @@
+package com.dmss.citysparkapplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PERSON")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    private Integer id;
+    private Integer user_id;
+    private String name;
+    private String contact_no;
+    private String description;
+    private LocalDateTime birthday;
+    private LocalDateTime created_dt;
+    private LocalDateTime updated_dt;
+
+}
