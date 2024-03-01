a13c15bfb1981b95bed05553ef0227e1995e5136
LeoChenJR <leochenjinran@gmail.com>
2024-03-02 01:29:25 +0800

    Resolve repository bean creating issue

--- back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/model/Event.java
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/model/Event.java
@@ -1,4 +1,11 @@
 package com.dmss.citysparkapplication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Event {
+    @Id
+    private Integer id;
+
 }
--- back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/repository/EventRepository.java
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/repository/EventRepository.java
@@ -3,6 +3,8 @@ package com.dmss.citysparkapplication.repository;
 //import org.springframework.data
 import com.dmss.citysparkapplication.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
