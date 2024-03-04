ae6aa32ab7dc34c5cef4c207469e73bf86ba70e4
LeoChenJR <leochenjinran@gmail.com>
2024-03-05 01:10:43 +0800

    Resolve warnings

--- back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/service/impl/PersonServiceImpl.java
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/service/impl/PersonServiceImpl.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class PersonServiceImpl implements PersonService {
