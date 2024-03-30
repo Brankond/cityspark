0056ae5fecfe2dfd3035de0ff2f5b785094d3634
Zhuoxin <lizhuoxinmike@gmail.com>
2024-03-30 22:09:39 +0800

    implement send notification when register an event

--- back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/service/ParticipationService.java
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/service/ParticipationService.java
@@ -1,5 +1,6 @@
 package com.dmss.citysparkapplication.service;
 
+import com.dmss.citysparkapplication.model.Notification;
 import com.dmss.citysparkapplication.model.Participation;
 
 public interface ParticipationService {
@@ -7,5 +8,7 @@ public interface ParticipationService {
     boolean registerEvent(Participation participation);
 
     public boolean cancelRegistration(Integer id);
+
+    Notification sendNotification(Participation participation, boolean register);
 
 }
