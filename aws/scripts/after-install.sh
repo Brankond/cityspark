c4eb4e54460f247f0081c59a30be553544770069
JikunLiu101 <1015482278@qq.com>
2024-03-30 21:22:58 +0800

    CIT-45 CI-CD setup

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://city-spark-stack-webappdeploymentbucket-pekrbprslirn/SpringBootHelloWorldExampleApplication.jar /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.jar
+aws s3 cp s3://city-spark-stack-webappdeploymentbucket-pekrbprslirn/citysparkApplication-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/citysparkApplication-0.0.1-SNAPSHOT.jar
 
 
 # Ensure the ownership permissions are correct.
