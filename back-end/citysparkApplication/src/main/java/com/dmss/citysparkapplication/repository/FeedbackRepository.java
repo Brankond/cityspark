e3d7e860373ff37c716a9067bcef187605586f7e
Zhuoxin <lizhuoxinmike@gmail.com>
2024-04-13 18:21:02 +0800

    implement feedback api

---
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.dmss.citysparkapplication.repository;
+
+import com.dmss.citysparkapplication.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+}
