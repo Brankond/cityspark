32877aa335cfc7d016f57796ec4b4cbba316b3f3
JikunLiu101 <1015482278@qq.com>
2024-04-14 21:47:14 +0800

    CIT-45 CI-CD setup

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/citysparkApplication-0.0.1-SNAPSHOT.war /usr/local/tomcat9/webapps/citysparkApplication-0.0.1-SNAPSHOT.war
+aws s3 cp s3://city-spark-s3/citysparkApplication-0.0.1-SNAPSHOT.war /usr/local/tomcat9/webapps/citysparkApplication-0.0.1-SNAPSHOT.war
 
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
