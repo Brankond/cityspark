053744ae5b67be9228043ab33276fc822e2434f9
JikunLiu101 <1015482278@qq.com>
2024-04-17 20:49:57 +0800

    Updated application-start.sh to start java application in backend with nohup and no output

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-# Start Tomcat, the application server.
-java -jar /usr/local/tomcat9/webapps/citysparkApplication-0.0.1-SNAPSHOT.war
+# Start java, the application server.
+nohup java -jar /usr/local/tomcat9/webapps/citysparkApplication-0.0.1-SNAPSHOT.war > /dev/null &
