e4b235133d8843f9a7f40d755028f8431fdaa56d
Brandon <>
2024-04-19 09:42:34 +0800

    [dev] Global CORS configuration

---
+++ back-end/citysparkApplication/src/main/java/com/dmss/citysparkapplication/configurations/WebConfig.java
@@ -0,0 +1,15 @@
+package com.dmss.citysparkapplication.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .exposedHeaders(new String[]{"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"});
+    }
+}
